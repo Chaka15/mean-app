03ec93b9bf07aeb725853ab9b6b4710b0aa4e79a
Predrag Carapic <pero0904@gmail.com>
2022-06-20 14:02:43 +0200

    add posts route and return example posts data

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'sdfsrr324',
+      title: 'First post',
+      content: 'Content from BE first',
+    },
+    {
+      id: 'gh2rrd325',
+      title: 'Second post',
+      content: 'Content from BE second',
+    },
+  ];
 
-app.use((req, res, next) => {
-  res.send('Hello from Express ');
+  res.status(200).json({
+    message: 'Posts fethced succesfully!',
+    posts,
+  });
 });
 
 module.exports = app;
